9712a5e8c722f83b78ff0359b2bb9b939e3900fd
Tim Cook <78813876+timcookk@users.noreply.github.com>
2022-07-14 21:31:34 -0500

    Create Graphs.py

---
+++ Graphs.py
@@ -0,0 +1,11 @@
+from utilities import vaccination_graph
+print(vaccination_graph())
+
+from utilities import vaccination_image
+print(vaccination_image())
+
+from utilities import get_covidcase_description
+print(get_covidcase_description())
+
+from utilities import vaccination_age
+print(vaccination_age())
